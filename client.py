01872ec984533958c4b3d9ae9de6f7fe2e524838
Minh Dao Nguyen <90809666+Minh-Dao-Nguyen@users.noreply.github.com>
2025-06-20 23:01:03 -0500

    Lab complete ? The test functions hang indefinitely.

--- client.py
+++ client.py
@@ -1,19 +1,72 @@
 import random
 import threading
 from typing import Any, List
-
+import time
 from labrpc.labrpc import ClientEnd
-from server import GetArgs, GetReply, PutAppendArgs, PutAppendReply
+from server import GetArgs, GetReply, PutAppendArgs, PutAppendReply, WrongShardException
 
 def nrand() -> int:
     return random.getrandbits(62)
-
+MAX_TRIES = 10
 class Clerk:
     def __init__(self, servers: List[ClientEnd], cfg):
         self.servers = servers
         self.cfg = cfg
 
         # Your definitions here.
+        self.num = nrand()
+        self.req_count = 0
+        self.replication = cfg.nreplicas
+        #print(len(servers))
+
+    def get(self, key: str) -> str:
+        self.req_count += 1
+        #shard = int(key) % self.cfg.nservers
+        shard = sum([ord(c) for c in key]) % self.cfg.nservers
+        servers = [(shard + i) % self.cfg.nservers for i in range(self.cfg.nreplicas)]
+        args = GetArgs(key, self.num, self.req_count)
+
+        for i in range(MAX_TRIES):
+            #for j in self.servers:
+            for idx in servers:
+                try:
+                    #reply = j.call("KVServer.Get", args)
+                    reply = self.servers[idx].call("KVServer.Get", args)
+                    if reply.value == "WRONG_SHARD":
+                        continue
+                    return reply.value
+                except KeyError :
+                    continue
+                except:
+                    continue
+            time.sleep(0.05) # for some reason this is needed or it return wrong value
+
+        return ""
+
+    def put_append(self, key: str, value: str, op: str) -> str:
+        self.req_count += 1
+        #shard = int(key) % self.cfg.nservers
+        shard = sum([ord(c) for c in key]) % self.cfg.nservers
+        servers = [(shard + i) % self.cfg.nservers for i in range(self.cfg.nreplicas)]
+        args = PutAppendArgs(key, value, self.num, self.req_count)
+
+        for i in range(MAX_TRIES):
+            #for j in self.servers:
+            for idx in servers:
+                try:
+                    #reply = j.call("KVServer." + op, args)
+                    reply = self.servers[idx].call("KVServer." + op, args)
+                    if reply.value == "WRONG_SHARD":
+                        continue
+                    return reply.value
+                except KeyError:
+                    continue
+                except:
+                    continue
+            time.sleep(0.05)
+
+        return ""
+
 
     # Fetch the current value for a key.
     # Returns "" if the key does not exist.
@@ -26,22 +79,97 @@ class Clerk:
     # The types of args and reply (including whether they are pointers)
     # must match the declared types of the RPC handler function's
     # arguments in server.py.
-    def get(self, key: str) -> str:
-        # You will have to modify this function.
-        return ""
-
-    # Shared by Put and Append.
+    # def get(self, key: str) -> str:
+    #     # You will have to modify this function.
+    #     #get_args = GetArgs(key, self.num, self.req_count)
+    #     #self.req_count += 1
+    #     #reply = self.server[i].call("KVServer.Get", get_args)
+    #     shard = int(key) % self.cfg.nservers
+    #     self.req_count += 1
+    #     tries = 0
+    #     while True:
+    #         #for i in self.servers:
+    #         for i in range(self.replication):
+    #             sid = (shard + i) % self.cfg.nservers
+    #             try:
+    #                 get_args = GetArgs(key, self.num, self.req_count)
+    #                 #reply = self.servers[0].call("KVServer.Get", get_args)
+    #                 #reply = i.call("KVServer.Get", get_args)
+    #                 reply = self.servers[sid].call("KVServer.Get", get_args)
+    #                 #print("called get key", key)
+    #                 return reply.value
+    #             except:
+    #                 continue
+    #         time.sleep(0.05)
+    #         tries += 1
+    #         if tries == 10000:
+    #             break
+    #         # for i in self.servers:
+    #         #     try:
+    #         #         get_args = GetArgs(key, self.num, self.req_count)
+    #         #         self.req_count += 1
+    #         #         reply = i.call("KVServer.Get", get_args)
+    #         #         print("called get key", key)
+    #         #         return reply.value
+    #         #     except:
+    #         #         continue
     #
-    # You can send an RPC with code like this:
-    # reply = self.servers[i].call("KVServer."+op, args)
-    # assuming that you are connecting to the i-th server.
+    # # Shared by Put and Append.
+    # #
+    # # You can send an RPC with code like this:
+    # # reply = self.servers[i].call("KVServer."+op, args)
+    # # assuming that you are connecting to the i-th server.
+    # #
+    # # The types of args and reply (including whether they are pointers)
+    # # must match the declared types of the RPC handler function's
+    # # arguments in server.py.
+    # def put_append(self, key: str, value: str, op: str) -> str:
+    #     # You will have to modify this function.
+    #     # put_append_arg = PutAppendArgs(key, value, self.num, self.req_count)
+    #     # self.req_count += 1
+    #     #reply = self.servers[i].call("KVServer." + op, put_append_arg)
+    #     # for i in self.servers:
+    #     #     try:
+    #     #         put_append_arg = PutAppendArgs(key, value, self.num, self.req_count)
+    #     #         self.req_count += 1
+    #     #         reply = i.call("KVServer." + op, put_append_arg)
+    #     #         check = True if value else False
+    #     #         print("call", op, key, value, check)
+    #     #         #if reply.value:
+    #     #         return reply.value
+    #     #     except:
+    #     #         continue
+    #     # return ""
     #
-    # The types of args and reply (including whether they are pointers)
-    # must match the declared types of the RPC handler function's
-    # arguments in server.py.
-    def put_append(self, key: str, value: str, op: str) -> str:
-        # You will have to modify this function.
-        return ""
+    #     #int(key) % nshards
+    #     shard = int(key) % self.cfg.nservers
+    #     self.req_count += 1
+    #     tries = 0
+    #     while True:
+    #         #for i in self.servers:
+    #         for i in range(self.replication):
+    #             sid = (shard + i) % self.cfg.nservers
+    #             try:
+    #                 put_append_arg = PutAppendArgs(key, value, self.num, self.req_count)
+    #                 #reply = self.servers[0].call("KVServer." + op, put_append_arg)
+    #                 #reply = i.call("KVServer." + op, put_append_arg)
+    #                 reply = self.servers[sid].call("KVServer." + op, put_append_arg)
+    #                 #check = True if value else False
+    #                 #print("call", op, key, value, check)
+    #                 # if reply.value:
+    #                 return reply.value
+    #             except:
+    #                 continue
+    #         time.sleep(0.05)
+    #         tries += 1
+    #         if tries == 10000:
+    #             break
+    #
+    #     # if op == "Put":
+    #     #     reply = self.servers[i].call("KVServer.Put", put_append_arg)
+    #     # elif op == "Append":
+    #     #     reply = self.servers[i].call("KVServer.Append", put_append_arg)
+    #     return ""
 
     def put(self, key: str, value: str):
         self.put_append(key, value, "Put")
--- server.py
+++ server.py
@@ -1,8 +1,15 @@
+#### COMMENTING """
+# I have used Generative AI for the last task ( sharding ) of the lab to generated the logic for
+#     the reponsible, replicate, and leader function. Then I debug with tests cases
+
 import logging
 import threading
-from typing import Tuple, Any
+from typing import Tuple, Any, List
 
 debugging = False
+
+class WrongShardException(Exception):
+    pass
 
 # Use this function for debugging
 def debug(format, *args):
@@ -12,9 +19,12 @@ def debug(format, *args):
 # Put or Append
 class PutAppendArgs:
     # Add definitions here if needed
-    def __init__(self, key, value):
+    def __init__(self, key, value, client_num, req_count, forwarded=False):
         self.key = key
         self.value = value
+        self.client_num = client_num
+        self.req_count = req_count
+        self.forwarded = forwarded
 
 class PutAppendReply:
     # Add definitions here if needed
@@ -23,38 +33,197 @@ class PutAppendReply:
 
 class GetArgs:
     # Add definitions here if needed
-    def __init__(self, key):
+    def __init__(self, key, client_num, req_count):
         self.key = key
+        self.client_num = client_num
+        self.req_count = req_count
 
 class GetReply:
     # Add definitions here if needed
     def __init__(self, value):
         self.value = value
-
 class KVServer:
     def __init__(self, cfg):
         self.mu = threading.Lock()
         self.cfg = cfg
 
-        # Your definitions here.
+        self.map_data = {}
+        self.req_data = {}
+
+        self.nshards = cfg.nservers
+        self.replication = cfg.nreplicas
+
+    def is_responsible(self, key: str) -> bool:
+        shard = sum([ord(c) for c in key]) % self.nshards
+        group = [(shard + i) % self.nshards for i in range(self.replication)]
+        return self in [self.cfg.kvservers[i] for i in group]
+
+    def is_leader(self, key: str) -> bool:
+        shard = sum([ord(c) for c in key]) % self.nshards
+        return self is self.cfg.kvservers[shard]
+
+    def replicate(self, method: str, args):
+        shard = sum([ord(c) for c in args.key]) % self.nshards
+        for i in range(1, self.replication):
+            sid = (shard + i) % self.nshards
+            try:
+                self.cfg.kvservers[sid].call("KVServer." + method, args)
+            except:
+                continue
 
     def Get(self, args: GetArgs):
+        if not self.is_responsible(args.key):
+            #return GetReply("WRONG_SHARD")
+            #raise Exception("WRONG_SHARD")
+            #raise WrongShardException("WRONG_SHARD")
+            raise KeyError("WRONG_SHARD")
+
         reply = GetReply(None)
-
-        # Your code here.
-
-        return reply
+        with self.mu:
+            reply.value = self.map_data.get(args.key, "")
+            return reply
 
     def Put(self, args: PutAppendArgs):
+        if not self.is_responsible(args.key):
+            #return PutAppendReply("WRONG_SHARD")
+            #raise Exception("WRONG_SHARD")
+            #raise WrongShardException("WRONG_SHARD")
+            raise KeyError("WRONG_SHARD")
+
         reply = PutAppendReply(None)
+        with self.mu:
+            if args.client_num in self.req_data:
+                last_req, last_reply = self.req_data[args.client_num]
+                if last_req == args.req_count:
+                    return last_reply
+                elif last_req > args.req_count:
+                    return reply
 
-        # Your code here.
+            if self.is_leader(args.key):
+                self.map_data[args.key] = args.value
+                self.req_data[args.client_num] = (args.req_count, reply)
+                self.replicate("Put", args)
 
-        return reply
+            return reply
+
 
     def Append(self, args: PutAppendArgs):
+        if not self.is_responsible(args.key):
+            #return PutAppendReply("WRONG_SHARD")
+            #raise Exception("WRONG_SHARD")
+            #raise WrongShardException("WRONG_SHARD")
+            raise KeyError("WRONG_SHARD")
+
         reply = PutAppendReply(None)
+        with self.mu:
+            if args.client_num in self.req_data:
+                last_req, last_reply = self.req_data[args.client_num]
+                if last_req == args.req_count:
+                    return last_reply
+                elif last_req > args.req_count:
+                    return reply
 
-        # Your code here.
+            if self.is_leader(args.key):
+                old_val = self.map_data.get(args.key, "")
+                self.map_data[args.key] = old_val + args.value
+                reply.value = old_val
+                self.req_data[args.client_num] = (args.req_count, reply)
+                self.replicate("Append", args)
 
-        return reply
+            return reply
+
+
+# class KVServer:
+#     def __init__(self, cfg):
+#         self.mu = threading.Lock()
+#         self.cfg = cfg
+#
+#         # Your definitions here.
+#         self.map_data = {}
+#         #self.req_data = []
+#         self.req_data = {}
+#
+#     def Get(self, args: GetArgs):
+#         reply = GetReply(None)
+#
+#         # Your code here.
+#         with self.mu:
+#             if args.key in self.map_data.keys():
+#                 value = self.map_data[args.key]
+#                 reply.value = value
+#             else:
+#                 reply.value = ""
+#
+#             # if args.client_num in self.req_data:
+#             #     # ignore how ?
+#             #     return reply
+#             # else:
+#             #     self.req_data.append(args.client_num)
+#
+#             if args.client_num in self.req_data.keys():
+#                 last_req, last_reply = self.req_data[args.client_num]
+#                 if last_req == args.req_count:
+#                     print("dup")
+#                     return last_reply
+#                 elif last_req > args.req_count:
+#                     print("dup")
+#                     return reply
+#
+#             return reply
+#
+#     def Put(self, args: PutAppendArgs):
+#         reply = PutAppendReply(None)
+#
+#         # Your code here.
+#         with self.mu:
+#             #if args.key in self.map_data.keys():
+#             # if args.client_num in self.req_data.keys():
+#             #     if self.req_data[args.client_num][0] >= args.req_count:
+#             #         return self.req_data[args.client_num][1]
+#             if args.client_num in self.req_data:
+#                 #print("found put")
+#                 last_req, last_reply = self.req_data[args.client_num]
+#                 if last_req == args.req_count:
+#                     #print("dup", "-"*30)
+#                     return last_reply
+#                 elif last_req > args.req_count:
+#                     #print("dup", "-"*30)
+#                     return PutAppendReply(None)
+#
+#             self.map_data[args.key] = args.value
+#             self.req_data[args.client_num] = (args.req_count, reply)
+#             #print("req_data add:", args.req_count, args.client_num)
+#             return reply
+#
+#     def Append(self, args: PutAppendArgs):
+#         reply = PutAppendReply(None)
+#
+#         # Your code here.
+#         with self.mu:
+#             # if args.client_num in self.req_data.keys():
+#             #     if self.req_data[args.client_num][0] >= args.req_count:
+#             #         return self.req_data[args.client_num][1]
+#             if args.client_num in self.req_data:
+#                 #print("found append")
+#                 last_req, last_reply = self.req_data[args.client_num]
+#                 if last_req == args.req_count:
+#                     #print("dup", "-"*30)
+#                     return last_reply
+#                 elif last_req > args.req_count:
+#                     #print("dup", "-"*30)
+#                     return PutAppendReply(None)
+#
+#             # if args.key in self.map_data.keys():
+#             #     old_val = self.map_data[args.key]
+#             #     self.map_data[args.key] = old_val + args.value
+#             #     reply.value = old_val
+#             # else:
+#             #     self.map_data[args.key] = args.value
+#             #     reply.value = ""
+#
+#             old_val = self.map_data.get(args.key, "")
+#             self.map_data[args.key] = old_val + args.value
+#             reply = PutAppendReply(old_val)
+#
+#             self.req_data[args.client_num] = (args.req_count, reply)
+#             return reply
